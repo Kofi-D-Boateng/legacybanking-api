f9a8916b3ba43d39160411cdcdc6f581ff6329e1
Kofi Boateng <kdboat2@gmail.com>
2023-02-22 16:28:26 -0600

    changed query to params toas query was not retrieving any of the keys from the url.

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,96 +1,143 @@
-"use strict"
+"use strict";
 import { Response, Request } from "express";
 import axios from "axios";
-import {LoginRequest, LoginRequestReturn} from "../types/LoginRequest";
+import { LoginRequest, LoginRequestReturn } from "../types/LoginRequest";
 import config from "../config/config";
 import { Registration } from "../types/Registration";
 
-export const loginCustomer:(req:Request,res:Response) => void = (req,res) =>{
-    const loginRequest:LoginRequest = req.body;
-    
-    if((!loginRequest.email|| loginRequest.email.trim().length <= 0) || (!loginRequest.password || loginRequest.password.trim().length <= 0)){
-        res.status(401);
-        return;
-    }
+export const loginCustomer: (req: Request, res: Response) => void = (
+  req,
+  res
+) => {
+  const loginRequest: LoginRequest = req.body;
 
-    axios.post(`${config.Microservices.Auth}${config.Routes.AuthService.loginUser}`,loginRequest).then((response)=>{
-        const returnValue:LoginRequestReturn = response.data;
-        res.status(response.status).json(returnValue)
+  if (
+    !loginRequest.email ||
+    loginRequest.email.trim().length <= 0 ||
+    !loginRequest.password ||
+    loginRequest.password.trim().length <= 0
+  ) {
+    res.status(401);
+    return;
+  }
 
-    }).catch((reason)=>{
-        console.log(reason["message"]);
-        res.status(reason["response"]["status"]).json("Unauthorized")
+  axios
+    .post(
+      `${config.Microservices.Auth}${config.Routes.AuthService.loginUser}`,
+      loginRequest
+    )
+    .then((response) => {
+      const returnValue: LoginRequestReturn = response.data;
+      res.status(response.status).json(returnValue);
     })
+    .catch((reason) => {
+      console.log(reason["message"]);
+      res.status(reason["response"]["status"]).json("Unauthorized");
+    });
+};
 
-}
+export const getRefreshToken: (req: Request, res: Response) => void = (
+  req,
+  res
+) => {
+  const token: string | undefined = req.get("authorization");
+  const apiKey = req.params["apiKey"];
 
-export const getRefreshToken:(req:Request,res:Response) => void = (req,res) =>{
-    const token:string|undefined = req.get("authorization");
-    const apiKey = req.query["apiKey"];
+  if (!token || token.trim().length <= 0 || !apiKey) {
+    res.status(401);
+    return;
+  }
 
-    if((!token || token.trim().length <= 0) || !apiKey){
-        res.status(401)
-        return;
-    }
-
-    axios.get(`${config.Microservices.Auth}/${config.Routes.AuthService.getRefreshToken}`,{headers:{"authorization":token},params:{"apiKey":apiKey}}).then((response)=>{
-        const returnValue:{token:string, expiresIn:number} = response.data;
-        if(!returnValue.token || returnValue.token.trim().length <=0){
-            throw new Error("invalid");
-        }
-        res.status(200).json(returnValue);
-    }).catch((reason)=>{
-        console.log(reason["message"]);
-        res.status(reason["response"]["status"]).json("Unauthorized")
+  axios
+    .get(
+      `${config.Microservices.Auth}/${config.Routes.AuthService.getRefreshToken}`,
+      { headers: { authorization: token }, params: { apiKey: apiKey } }
+    )
+    .then((response) => {
+      const returnValue: { token: string; expiresIn: number } = response.data;
+      if (!returnValue.token || returnValue.token.trim().length <= 0) {
+        throw new Error("invalid");
+      }
+      res.status(200).json(returnValue);
     })
-}
+    .catch((reason) => {
+      console.log(reason["message"]);
+      res.status(reason["response"]["status"]).json("Unauthorized");
+    });
+};
 
-export const logoutCustomer:(req:Request,res:Response) => void = (req,res) =>{
-    const token:string|undefined = req.get("authorization");
-    const apiKey = req.query["apiKey"];
-    if((!token || token.trim().length <= 0) || !apiKey){
-        res.status(401).json("Unauthorized")
-        return;
-    }
+export const logoutCustomer: (req: Request, res: Response) => void = (
+  req,
+  res
+) => {
+  const token: string | undefined = req.get("authorization");
+  const apiKey = req.query["apiKey"];
+  if (!token || token.trim().length <= 0 || !apiKey) {
+    res.status(401).json("Unauthorized");
+    return;
+  }
 
-    axios.get(`${config.Microservices.Auth}/${config.Routes.AuthService.logoutUser}`,{headers:{"authorization":token},params:{apiKey:apiKey}})
-    .then(()=> res.status(200).json(""))
-    .catch((reason)=> {
-        console.log(reason["message"]);
-        res.status(reason["response"]["status"]).json("Unauthorized")
-    })
-    
-}
+  axios
+    .get(
+      `${config.Microservices.Auth}/${config.Routes.AuthService.logoutUser}`,
+      { headers: { authorization: token }, params: { apiKey: apiKey } }
+    )
+    .then(() => res.status(200).json(""))
+    .catch((reason) => {
+      console.log(reason["message"]);
+      res.status(reason["response"]["status"]).json("Unauthorized");
+    });
+};
 
-export const confirmCustomerRegistration:(req:Request,res:Response) => void = (req,res) =>{
-    const token:string | undefined = req.params["token"]
+export const confirmCustomerRegistration: (
+  req: Request,
+  res: Response
+) => void = (req, res) => {
+  const token: string | undefined = req.params["token"];
 
-    if(!token || token.trim().length <= 0){
-        res.status(401);
-        return;
-    }
+  if (!token || token.trim().length <= 0) {
+    res.status(401);
+    return;
+  }
 
-    axios.post(`${config.Microservices.Auth}/${config.Routes.AuthService.confirmCustomerRegistration}`,{token:token})
-    .then(()=> res.status(200))
-    .catch((reason)=> {
-        console.log(reason["message"]);
-        res.status(reason["response"]["status"]).json("Unauthorized")
-    })
-}
-
-export const registerCustomer:(req:Request,res:Response) => void = (req,res) =>{
-    const authToken:string|undefined = req.get("authorization");
-    const registrationRequest:Registration = req.body;
-
-    if((!registrationRequest.employeeId || registrationRequest.employeeId.trim().length <= 0) || (!authToken || authToken.trim().length <= 0)){
-        res.status(401);
-        return;
-    }
-
-    axios.post(`${config.Microservices.Auth}/${config.Routes.AuthService.registerCustomer}`,registrationRequest,{headers:{"authorization":authToken}})
+  axios
+    .post(
+      `${config.Microservices.Auth}/${config.Routes.AuthService.confirmCustomerRegistration}`,
+      { token: token }
+    )
     .then(() => res.status(200))
-    .catch((reason)=>{
-        console.log(reason["message"]);
-        res.status(reason["response"]["status"]).json("Unauthorized")
-    })
-}
+    .catch((reason) => {
+      console.log(reason["message"]);
+      res.status(reason["response"]["status"]).json("Unauthorized");
+    });
+};
+
+export const registerCustomer: (req: Request, res: Response) => void = (
+  req,
+  res
+) => {
+  const authToken: string | undefined = req.get("authorization");
+  const registrationRequest: Registration = req.body;
+
+  if (
+    !registrationRequest.employeeId ||
+    registrationRequest.employeeId.trim().length <= 0 ||
+    !authToken ||
+    authToken.trim().length <= 0
+  ) {
+    res.status(401);
+    return;
+  }
+
+  axios
+    .post(
+      `${config.Microservices.Auth}/${config.Routes.AuthService.registerCustomer}`,
+      registrationRequest,
+      { headers: { authorization: authToken } }
+    )
+    .then(() => res.status(200))
+    .catch((reason) => {
+      console.log(reason["message"]);
+      res.status(reason["response"]["status"]).json("Unauthorized");
+    });
+};
--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -1,54 +1,69 @@
-"use strict"
+"use strict";
 
-import axios from "axios"
-import { Request,Response } from "express";
+import axios from "axios";
+import { Request, Response } from "express";
 import config from "../config/config";
 import { Customer } from "../types/Customer";
-import { _getEmailFromCache, _getUserFromCache, _saveUserToCache } from "../utils/redis/query";
+import {
+  _getEmailFromCache,
+  _getUserFromCache,
+  _saveUserToCache,
+} from "../utils/redis/query";
 
-export const getProfile:(req:Request, res:Response) => void = async (req,res) =>{
-    const token:string | undefined = req.get("authorization");
-    const apiKey = req.query["apiKey"];
+export const getProfile: (req: Request, res: Response) => void = async (
+  req,
+  res
+) => {
+  const token: string | undefined = req.get("authorization");
+  const apiKey = req.params["apiKey"];
 
-    if(!token || !apiKey){
-        res.status(401).json("Unauthorized");
-        return;
+  if (!token || !apiKey) {
+    res.status(401).json("Unauthorized");
+    return;
+  }
+  try {
+    const authResponse = await axios.get(
+      `${config.Microservices.Auth}${config.Routes.AuthService.authenticateUser}`,
+      { params: { token: token } }
+    );
+    if (authResponse.status != 200) {
+      throw new Error(authResponse.data);
     }
-    try {
-        const authResponse = await axios.get(`${config.Microservices.Auth}${config.Routes.AuthService.authenticateUser}`,{params:{"token":token}});
-        if(authResponse.status != 200){
-            throw new Error(authResponse.data);
-        }
-        const customer:Customer | null = await _getUserFromCache(apiKey as string);
-        if(!customer){
-            const returnedEmailValue = await _getEmailFromCache(apiKey as string);
-            if(returnedEmailValue instanceof Error){
-                throw returnedEmailValue
-            }
-            const bankResponse = await axios.get(`${config.Microservices.Bank}${config.Routes.BankingService.getFullCustomerInfo}`,{params:{"username":returnedEmailValue}})
-            if(bankResponse.status != 200){
-                throw new Error(bankResponse.data)
-            }
+    const customer: Customer | null = await _getUserFromCache(apiKey as string);
+    if (!customer) {
+      const returnedEmailValue = await _getEmailFromCache(apiKey as string);
+      if (returnedEmailValue instanceof Error) {
+        throw returnedEmailValue;
+      }
+      const bankResponse = await axios.get(
+        `${config.Microservices.Bank}${config.Routes.BankingService.getFullCustomerInfo}`,
+        { params: { username: returnedEmailValue } }
+      );
+      if (bankResponse.status != 200) {
+        throw new Error(bankResponse.data);
+      }
 
-            const notifResponse = await axios.get(`${config.Microservices.Notifications}${config.Routes.NotificationService.getNotifications}`,{params:{"email":returnedEmailValue}});
-            if(notifResponse.status != 200){
-                throw new Error(notifResponse.data);
-            }
-            const newCustomer:Customer = bankResponse.data;
-            newCustomer.notifications = notifResponse.data;
-            await _saveUserToCache(apiKey as string,newCustomer);
-            res.status(200).json(newCustomer);
-            return;
-        }
-        res.status(200).json(customer);
-    } catch (error:any) {
-        if(!error["message"]){
-            console.log(error)
-            res.status(401).json("")
-        }else{
-            console.log(error["message"])
-            res.status(error["response"]["status"]).json("")
-        }
-        
-    }    
-}
+      const notifResponse = await axios.get(
+        `${config.Microservices.Notifications}${config.Routes.NotificationService.getNotifications}`,
+        { params: { email: returnedEmailValue } }
+      );
+      if (notifResponse.status != 200) {
+        throw new Error(notifResponse.data);
+      }
+      const newCustomer: Customer = bankResponse.data;
+      newCustomer.notifications = notifResponse.data;
+      await _saveUserToCache(apiKey as string, newCustomer);
+      res.status(200).json(newCustomer);
+      return;
+    }
+    res.status(200).json(customer);
+  } catch (error: any) {
+    if (!error["message"]) {
+      console.log(error);
+      res.status(401).json("");
+    } else {
+      console.log(error["message"]);
+      res.status(error["response"]["status"]).json("");
+    }
+  }
+};
