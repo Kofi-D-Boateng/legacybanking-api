bbd5da129b0880ae5c85cf3b606064b26b99bffe
Kofi Boateng <84227601+Kofi-D-Boateng@users.noreply.github.com>
2023-02-22 16:33:32 -0600

    changed query to params toas query was not retrieving any of the keys from the url. (#6)

--- src/controllers/aws.ts
+++ src/controllers/aws.ts
@@ -1,30 +1,33 @@
-"use strict"
+"use strict";
 
-import { Response,Request } from "express"
+import { Response, Request } from "express";
 import { Readable } from "stream";
 import { _getVideoFromS3Bucket } from "../utils/aws/s3";
 import { _getVideoSrcFromCache, _saveSrcToCache } from "../utils/redis/query";
 
+export const s3Bucket: (req: Request, res: Response) => void = async (
+  req,
+  res
+) => {
+  const videoName = req.params["key"];
+  if (!videoName || videoName.trim().length <= 0) {
+    console.log("[ERROR]: Video tags not being sent from frontend....");
+    res.status(400);
+    return;
+  }
 
-export const s3Bucket:(req:Request, res:Response) => void = async (req,res) =>{
-    const videoName = req.query["key"] as string | undefined
-    if(!videoName || videoName.trim().length <= 0){
-        console.log("[ERROR]: Video tags not being sent from frontend....");
-        res.status(400)
-        return
+  const videoSrc: Readable | null = await _getVideoSrcFromCache(videoName);
+
+  if (!videoSrc) {
+    const readableStream: Readable | null = await _getVideoFromS3Bucket(
+      videoName
+    );
+    if (!readableStream) {
+      res.status(400);
+      return;
     }
-
-    const videoSrc:Readable | null = await _getVideoSrcFromCache(videoName)
-
-    if(!videoSrc){
-        const readableStream: Readable | null = await _getVideoFromS3Bucket(videoName);
-        if(!readableStream){
-            res.status(400);
-            return;
-        }
-        readableStream.pipe(res)
-        return
-    }
-    videoSrc.pipe(res);
-
-}
+    readableStream.pipe(res);
+    return;
+  }
+  videoSrc.pipe(res);
+};
